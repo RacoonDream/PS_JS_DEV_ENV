78be990cb585ed41ea9bad39b80da93868e76805
chikucoder <chikucoder@gmail.com>
2018-12-08 06:29:41 -0500

    delete - fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-import {getUsers} from './api/userApi';
+import {getUsers,deleteUser} from './api/userApi';
 
 const courseValue= numeral(1000).format('$0,0.00');
 //debugger;
@@ -13,6 +13,7 @@ getUsers().then(result=>{
  result.forEach(user=>{
 console.log(user.firstName);
  usersBody += `<tr>
+ <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
  <td>${user.id}</td>
  <td>${user.firstName}</td>
  <td>${user.lastName}</td>
@@ -22,4 +23,18 @@ console.log(user.firstName);
 
  global.document.getElementById('users').innerHTML=usersBody;
 
+
+ const deleteLinks= global.document.getElementsByClassName('deleteUser');
+
+ Array.from(deleteLinks, link=>{
+link.onclick= function(event){
+  const element = event.target;
+  event.preventDefault();
+  deleteUser(element.attributes["data-id"].value);
+
+  const row = element.parentNode.parentNode;// reach <tr>
+  row.parentNode.removeChild(row);// reach <table>
+
+};
+ })
 });
